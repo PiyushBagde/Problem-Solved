51d42702596147ba4f66f5aee9d30898380e719e
PiyushBagde <piyushbagde002@gmail.com>
2023-06-30 00:47:45 +0530

    added board

--- Assignment_01.py
+++ Assignment_01.py
@@ -11,9 +11,12 @@ gameRunning = True
 
 # print game board
 def printBoard(board):
-    print('|' + board[0] + '|' + board[1] + '|' + board[2] + '|')
-    print('|' + board[3] + '|' + board[4] + '|' + board[5] + '|')
-    print('|' + board[6] + '|' + board[7] + '|' + board[8] + '|')
+    print('| ' + board[0] + ' | ' + board[1] + ' | '  + board[2] + ' |')
+    print("-"*13)
+    print('| ' + board[3] + ' | ' + board[4] + ' | ' + board[5] + ' |')
+    print("-"*13)
+    print('| ' + board[6] + ' | ' + board[7] + ' |'  + board[8] + '  |')
+    
     
 printBoard(board)
     
