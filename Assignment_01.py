7850992fd0a669068fd8aacbfd56d8a64d7d9b0b
PiyushBagde <piyushbagde002@gmail.com>
2023-06-30 01:30:18 +0530

    added conditions

--- Assignment_01.py
+++ Assignment_01.py
@@ -29,11 +29,52 @@ def playerInput():
 
 
 #check for win or tie
+def checkRow(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != "-":
+       winner = board[0]
+       return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+    
+def checkColumn(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+       winner = board[0]
+       return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+    
+def checkDiagonal(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+       winner = board[0]
+       return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+    
+def checkTie(board):
+    global gameRunning
+    if "-" not in board:
+        printBoard(board)
+        print("It's a tie.")
+        gameRunning = False
 
 #switch the player
-
-#check for win or tie
 
 while gameRunning:
     printBoard(board)
     playerInput()
+    # checkRow(board)
+    # checkColumn(board)
+    # checkDiagonal(board)
+    # checkColumn(board)
