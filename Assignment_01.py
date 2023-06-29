ba042c7d10a980a62e0095898599a26e33d80956
PiyushBagde <piyushbagde002@gmail.com>
2023-06-30 00:42:15 +0530

    added global variables

---
+++ Assignment_01.py
@@ -0,0 +1,23 @@
+board = ["-", "-", "-",
+         "-","-","-",
+         "-","-","-",]
+
+currentPlayer = 'X'
+
+winner = None
+
+gameRunning = True
+
+
+# print game board
+
+    
+# printBoard(board)
+    
+#take player input
+
+#check for win or tie
+
+#switch the player
+
+#check for win or tie
