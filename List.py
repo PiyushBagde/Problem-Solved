6a825e888d3067c26a62a295fe11a2e0ec2d6a12
PiyushBagde <piyushbagde002@gmail.com>
2023-07-05 14:28:36 +0530

    instruction wise operation on array

---
+++ List.py
@@ -0,0 +1,28 @@
+N = int(input("Enter N: "))
+
+arr = []
+commands = []
+
+def action(row):
+    if row[0] == "insert":
+        arr.insert(row[2], int(row[1]))
+
+for i in range(N):
+    inst = input("Enter instruction: ")
+    commands.append([word for word in inst.split(" ")])
+
+for row in commands:
+    if "insert" in row:
+        arr.insert(int(row[1]), int(row[2]))
+    elif("append" in row):
+        arr.append(int(row[1]))
+    elif "remove" in row:
+        arr.remove(int(row[1]))
+    elif "sort" in row:
+        arr.sort()
+    elif "pop" in row:
+        arr.pop()
+    elif "reverse" in row:
+        arr = arr[::-1]
+    else:
+        print(arr)
