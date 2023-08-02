6179e2a1093792b1d63634c160006f6d8487aa88
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2023-08-02 11:32:06 +0530

    set difference

---
+++ set_difference.py
@@ -0,0 +1,19 @@
+
+english = set()
+french = set()
+
+n_eng = int(input("Enter no english lerner : "))
+eng_student = input("Enter students roll number : ")
+
+for student in eng_student.split(" "):
+    english.add(student)
+
+
+n_fren = int(input("Enter no of french lerner : "))
+fre_student = input("Enter student roll number : ")
+
+for student1 in fre_student.split(" "):
+    french.add(student1)
+
+print(len(english.difference(french)))
+    
