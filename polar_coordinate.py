93c2ba29f63f8a8ed028c2bfabbbdb31d6fd1122
PiyushBagde <piyushbagde002@gmail.com>
2023-08-05 20:50:06 +0530

    polar coordinates

---
+++ polar_coordinate.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+com_num = complex(input("Enter a complex number : "))
+print('absoloute value = ',abs(com_num))
+print('arguement = ',cmath.phase(com_num))
