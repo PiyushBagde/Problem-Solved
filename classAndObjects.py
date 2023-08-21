b38e42e975086897a76bfc5ea860398bddfc2b77
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2023-08-21 17:04:39 +0530

    Add files via upload

---
+++ classAndObjects.py
@@ -0,0 +1,15 @@
+class Squirrel:
+    name=''
+    age=0
+    
+Squirrel1 = Squirrel()
+Squirrel2 = Squirrel()
+
+Squirrel1.name = 'nemo'
+Squirrel1.age = 3
+
+Squirrel2.name = 'kiwi'
+Squirrel2.age = 1
+
+print(f"I am {Squirrel1.name} and I am {Squirrel1.age} years old.")
+print(f"I am {Squirrel2.name} and I am {Squirrel2.age} years old.")
