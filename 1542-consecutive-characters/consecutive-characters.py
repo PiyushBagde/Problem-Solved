4f1ce3c2da10c57f26b19f18e9b237230486c32b
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-01-27 13:53:03 +0530

    Time: 20 ms (77.92%) | Memory: 11.5 MB (99.35%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.py
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -1,20 +1,23 @@
-class Solution:
-    def maxPower(self, s: str) -> int:
-            if len(s) == 1:
+class Solution(object):
+    def maxPower(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 1:
                 return 1
-            else:
-                count = 1
-                max_count = 0
+        else:
+            count = 1
+            max_count = 0
 
-                for i in range(1, len(s)):
-                    if s[i] == s[i-1]:
-                        count += 1
-                        if count > max_count:
-                            max_count = count
-                    else:
-                        count = 1
-                if max_count == 0:
-                    return 1
+            for i in range(1, len(s)):
+                if s[i] == s[i-1]:
+                    count += 1
+                    if count > max_count:
+                        max_count = count
+                else:
+                    count = 1
+            if max_count == 0:
+                return 1
 
-            return max_count   
-            
+            return max_count 
