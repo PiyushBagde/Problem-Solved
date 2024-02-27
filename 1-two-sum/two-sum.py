c94d24c8a66359ff3431a88b6a79c30e7cb56e60
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-02-27 23:18:47 +0530

    Time: 62 ms (59.69%) | Memory: 17.8 MB (48.18%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-                i                
-        
+        temp = {}
+
+        for index, value in enumerate(nums):
+            diff = target - value
+
+            if diff in temp:
+                return [index, temp[diff]]
+            else:
+                # save the visited number which is not anser yet
+                temp[value] = index
