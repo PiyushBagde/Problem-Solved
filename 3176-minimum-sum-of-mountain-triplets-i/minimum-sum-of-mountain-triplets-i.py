410099557a092f10d67568beae767327b3753e04
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-06-13 14:26:51 +0530

    Time: 70 ms (42.97%) | Memory: 16.5 MB (89.51%) - LeetSync

--- 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.py
+++ 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.py
@@ -1,15 +1,17 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
-        count = []
+        count = 150
         for i in range(len(nums)-2):
             for j in range(i+1, len(nums)-1):
                 for k in range(j+1 , len(nums)):
                     if nums[i] < nums[j] and nums[j] > nums[k]:
-                        count.append(nums[i] + nums[j] + nums[k])
+                        total = nums[i]+nums[j]+nums[k]
+                        if count > total:
+                            count = total
         
-        if len(count) != 0:
-            return min(count)
-        return -1
+        if count == 150:
+            return -1
+        return count
 
 
     
