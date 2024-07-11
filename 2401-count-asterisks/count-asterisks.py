89b8951ceb35dd6766e7418e8747c007b7e80cfc
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-07-11 16:30:26 +0530

    Time: 40 ms (27.92%) | Memory: 16.6 MB (37.52%) - LeetSync

--- 2401-count-asterisks/count-asterisks.py
+++ 2401-count-asterisks/count-asterisks.py
@@ -1,15 +1,12 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        flag = 0 
+        flag = False
         count = 0
         for i in s:
-            if i == "*" and flag == 0:
+            if i == "*" and flag == False:
                 count += 1
             elif i == "|":
-                if flag == 0:
-                    flag = 1
-                else:
-                    flag = 0
+                flag = not flag
         return count
             
 
