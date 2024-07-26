0ab2df1254794cf2a0844aa909211e7bc2d92b24
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-07-26 19:34:26 +0530

    Time: 33 ms (77.22%) | Memory: 16.4 MB (97.14%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,22 +1,23 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        num = str(x)
-        flag = 0
-        rev = 0
-        max_num = 2**31 -1
-        
-        if num[0] == "-":
-            flag = 1
-            num = num[1:]
-        
-        rev = int(num[::-1])
-        if rev >= max_num:
-            return 0
-        
-        if flag == 1:
-            return -rev
+        res = 0
+        if x < 0:
+            flag  = 1
+            x = x * -1
         else:
-            return rev
-            
+            flag = 0
+        while x > 0:
+            rem = x % 10
+            res = (res * 10) + rem 
+            x = x // 10
+        
+        if (-2**31) <= res <=( 2**31) - 1:
+            if flag == 1:
+                return res * -1
+            else:
+                return res
+        else:
+            return 0
 
+        
         
