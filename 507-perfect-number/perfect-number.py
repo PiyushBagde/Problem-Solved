0a73a701fe802fd6bbe849919c03e5c1a00a5af3
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-07-26 19:02:25 +0530

    Time: 54 ms (9.29%) | Memory: 16.6 MB (10.58%) - LeetSync

---
+++ 507-perfect-number/perfect-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        total = 0
+        for i in range(1, ceil(sqrt(num))):
+            if num % i == 0:
+                total += i
+            if num % (num/i) == 0 and num != num/i:
+                total += num/i
+
+        return total == num 
+
