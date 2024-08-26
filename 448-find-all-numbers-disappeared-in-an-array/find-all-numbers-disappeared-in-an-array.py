7a9f302513c502368eb607c08e4eece97571cd77
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-08-26 21:33:31 +0530

    Time: 537 ms (5.18%) | Memory: 25 MB (61.04%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,11 +1,28 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        hashmap =  set(nums)
-        res = []
+        nums.sort()
+        res = []            
+
+        def search(nums, key):
+            low = 0 
+            high = len(nums)-1
+            while low<=high:
+                mid = (low+high)//2
+                if nums[mid] == i:
+                    return True
+                elif i < nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+
+            return False
+
+            
 
         for i in range(1, len(nums)+1):
-            if i not in hashmap:
+            if search(nums,i) == False:
                 res.append(i)
+                
         return res
 
 
