828b7379c680d70f6ab09572d656c8015d6f1d52
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-12-04 00:18:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (6.08%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,19 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        size = len(digits) - 1
 
-        digits[size] += 1
+        for i in range(len(digits) -1, -1, -1):
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
 
-        for i in range(size, 0, -1):
-            if digits[i] == 10:
-                digits[i] = 0
-                digits[i-1] += 1
-            else:
-                break
+            digits[i] = 0
 
-        if digits[0] == 10:
-            digits[0] = 0
-            digits.insert(0, 1)
+            if i == 0:
+                return [1] + digits
 
-        return digits
-            
+
+
+        
