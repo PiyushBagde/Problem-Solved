840a0587ce596e6d0fe656321b4448345196eced
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2025-01-12 22:56:44 +0530

    Time: 37 ms (25.20%) | Memory: 48.8 MB (21.05%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        
+        unordered_set<int> hashset;
         vector<int> res;
 
-        for( auto n : nums){
-            n = abs(n);
-
-            if ( nums[ n-1 ] < 0) {
-                res.push_back(n);
+        for (int i=0; i < nums.size(); i ++) {
+            if ( hashset.contains(nums[i])) {
+                res.push_back(nums[i]);
             }
             else {
-                nums[ n-1 ] = -nums[ n-1 ];
+                hashset.insert(nums[i]);
             }
         }
-        
+
         return res;
+
+
+        
     }
 };
